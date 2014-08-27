87610fe2047fe7f100c47b4ef581994cccd39749
Henk Raamkozijn <koen5067@icloud.com>
2014-08-27 14:18:08 +0200

    pauze script

---
+++ Assets/Scripts/Pauze.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pauze : MonoBehaviour {
+
+	private bool paused = false;
+	private int difficult = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.P)) 
+		{
+			if(paused)
+			{
+				paused = false;
+				Time.timeScale = difficult;
+			}
+			else
+			{
+				paused = true;
+				Time.timeScale = 0;
+			}
+		}
+	}
+}
