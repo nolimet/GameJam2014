b47521d671d210f6ffc85bea87e6177b2c6f241e
Henk Raamkozijn <koen5067@icloud.com>
2014-08-28 13:54:33 +0200

    Pauze komt in het scherm te staan

--- Assets/Scripts/misc/Pauze.cs
+++ Assets/Scripts/misc/Pauze.cs
@@ -1,27 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(TextMesh))]
 public class Pauze : MonoBehaviour {
 
 	private bool paused = false;
 	private int difficult = 1;
+	TextMesh txme;
 
-	void Update () {
+	void Start ()
+	{
+		txme = GetComponent<TextMesh>();
+	}
+
+	void Update () 
+	{
 		if (Input.GetKeyUp (KeyCode.P)) 
 		{
 			if(paused)
 			{
 				paused = false;
 				Time.timeScale = difficult; //spel gaat door op de vooraf gestelde snelheid
-				GameObject.Find ("audio source Muted").audio.Play();
+				//GameObject.Find ("audio source Muted").audio.Play();
 				GameObject.Find ("Directional light").light.intensity = 0.28f;
+				txme.text = " ";
 			}
 			else
 			{
+
 				paused = true;
 				Time.timeScale = 0;			//spel stopt tijdelijk tot je weer opnieuw op p drukt
-				GameObject.Find ("audio source Muted").audio.Pause();
+				//GameObject.Find ("audio source Muted").audio.Pause();
 				GameObject.Find ("Directional light").light.intensity = 0.1f;
+				txme.text = "Paused";
 
 			}
 		}
