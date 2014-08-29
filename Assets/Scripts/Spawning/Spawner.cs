a24863ac5d103aea4f3f9a30afa45a5405be942d
jscotty <jscotty@hotmail.nl>
2014-08-29 11:05:24 +0200

    merging

--- Assets/Scripts/Spawning/Spawner.cs
+++ Assets/Scripts/Spawning/Spawner.cs
@@ -5,55 +5,53 @@ public class Spawner : MonoBehaviour {
 	public Transform enemy;
 	private float time;
 	private float spawnHighCounter;
-	private int spawnTimer;
+	private float spawnTimer;
+	private int spawnTimer2;
+	private int spawnTimer3;
+	private int spawnTimer4;
 	public GameObject[] enemyList;
 
 	private int corner;
 
-    void Start()
-    {
-        
-    }
-
 	void Update(){
-		time += Time.deltaTime;
-		if (time >= 30) {
-			spawnHighCounter = 9;		
-		}
+		//print ("time" + time);
+		//spawnTimer += Time.deltaTime;
+		if (soundCheck.spawner == false) {
+			spawnTimer = 0;
+			spawnTimer2 = 0;
+			spawnTimer3 = 0;
+			spawnTimer4 = 0;
+		} //skrillex
 		if (soundCheck.spawner == true) {
 			spawnTimer += 1;
 			corner = 0;
 
-			if(spawnTimer >= 4)
-				Spawn();
-		}
+			if(spawnTimer == 1)Spawn();
+		} //marlyn manson
 		if (soundCheck.spawner2 == true) {
-			spawnTimer += 1;
-			corner = 1;
-			
-			if(spawnTimer >= 4)
-				Spawn();
-		}
-		if (soundCheck.spawner3 == true) {
-			spawnTimer += 1;
+			spawnTimer2 += 1;
 			corner = 2;
 
-			if(spawnTimer >= 4)
-				Spawn();
-		}
+			if(spawnTimer2 == 1)Spawn();
+		} //angerfist
+		if (soundCheck.spawner3 == true) {
+			corner = 1;
+			spawnTimer3 ++;
+
+			if(spawnTimer3 == 1)Spawn();
+		} //lil wayn
 		if (soundCheck.spawner4 == true) {
-			spawnTimer += 1;
+			spawnTimer4 += 1;
 			corner = 3;
 			
-			if(spawnTimer >= spawnHighCounter)
-				Spawn();
+			if(spawnTimer4 == 1)Spawn();
 		}
+		//print (spawnTimer3);
 	}
 
 	void Spawn(){
-		spawnTimer = 0;
 		Vector3 location = new Vector3 (-5.971444f, 4.378914f, 2f);
-		Vector3 locationTwo = new Vector3 (6.042645f, 4.378914f, 2f);
+		Vector3 locationTwo = new Vector3 (6.042645f, 4.378914f, 0.5f);
 		Vector3 locationThree = new Vector3 (5.901252f, -2.569977f, 2f);
 		Vector3 locationFour = new Vector3 (-5.241997f, -2.786465f, 2f);
 
--- Assets/Scripts/soundCheck.cs
+++ Assets/Scripts/soundCheck.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+[RequireComponent(typeof(AudioSource))]
 public class soundCheck : MonoBehaviour {
-	private float counter = 0;
+	private float counter;
+	private float counterFilter;
+	private float counterLast = 0f;
 	public static bool spawner;
 	public static bool spawner2;
 	public static bool spawner3;
@@ -14,49 +18,189 @@ public class soundCheck : MonoBehaviour {
 	private float freqMidTwo;
 	private float freqHigh;
 
-	void Update() {
+	void FixedUpdate() {
+		spawner = false;
+		spawner2 = false;
+		spawner3 = false;
+		spawner4 = false;
+
+		counterFilter +=  1f ;
+		counter = Mathf.Floor (counterFilter);
+		//print (counter);
+
+		if (counter == 40f){
+			spawner3 = true;
+		}
+		else if (counter == 70f){
+			spawner3 = true;
+		}
+		else if (counter == 102f){
+			spawner3 = true;
+		}
+		else if (counter == 162f){
+			spawner3 = true;
+		}
+		else if (counter == 192f){
+			spawner3 = true;
+		}
+		else if (counter == 213f){
+			spawner3 = true;
+		}
+		else if (counter == 280f){
+			spawner3 = true;
+		}
+		else if (counter == 310f){
+			spawner3 = true;
+		}
+		else if (counter == 337f){
+			spawner3 = true;
+		}
+		else if (counter == 380f){
+			spawner3 = true;
+		}
+		else if (counter == 415f){
+			spawner3 = true;
+		}
+		else if (counter == 445f){
+			spawner3 = true;
+		}
+		else if (counter == 480f){
+			spawner3 = true;
+		}
+		else if (counter == 502f){
+			spawner3 = true;
+		}
+		else if (counter == 542f){
+			spawner3 = true;
+		}
+		else if (counter == 552f){
+			spawner3 = true;
+		}
+		else if (counter == 558f){
+			spawner3 = true;
+		}
+		else if (counter == 562f){
+			spawner3 = true;
+		}
+		
+			else if (counter == 610f){
+				spawner4 = true;
+			}
+			else if (counter == 622f){
+				spawner4 = true;
+			}
+			else if (counter == 642f){
+				spawner4 = true;
+			}
+			else if (counter == 652f){
+				spawner4 = true;
+			}
+			else if (counter == 678f){
+				spawner4 = true;
+			}
+			else if (counter == 692f){
+				spawner4 = true;
+			}
+			else if (counter == 710f){
+				spawner4 = true;
+			}
+			else if (counter == 722f){
+				spawner4 = true;
+			}
+			else if (counter == 742f){
+				spawner4 = true;
+			}
+			else if (counter == 752f){
+				spawner4 = true;
+			}
+			else if (counter == 778f){
+				spawner4 = true;
+			}
+			else if (counter == 792f){
+				spawner4 = true;
+			}
+			else if (counter == 810f){
+				spawner4 = true;
+			}
+			else if (counter == 822f){
+				spawner4 = true;
+			}
+			else if (counter == 842f){
+				spawner4 = true;
+			}
+			else if (counter == 852f){
+				spawner4 = true;
+			}
+			else if (counter == 878f){
+				spawner4 = true;
+			}
+			else if (counter == 892f){
+				spawner4 = true;
+			}
+			else if (counter == 910f){
+				spawner4 = true;
+			}
+			else if (counter == 922f){
+				spawner4 = true;
+			}
+			else if (counter == 942f){
+				spawner4 = true;
+			}
+			else if (counter == 952f){
+				spawner4 = true;
+			}
+			else if (counter == 978f){
+				spawner4 = true;
+			}
+			else if (counter == 992f){
+				spawner4 = true;
+			}
+		
+		print (counter);
 
 		/*om de frequenties van de spectrum wave van de audio gebruik je GetspectrumData*/
-		freqData = AudioListener.GetSpectrumData (nSamples, 0, FFTWindow.Hamming);
+		/*freqData = AudioListener.GetSpectrumData (nSamples, 0, FFTWindow.Hamming);
 
-		float low = freqData [3] + freqData [2] + freqData [4];
-		float mid = freqData [11] + freqData [12] + freqData [13];
-		float midTwo = freqData [22] + freqData [23] + freqData [24];
-		float high = freqData [44] + freqData [45] + freqData [46] + freqData [47] + freqData [48] + freqData [49];
+		float low = freqData [3] + freqData [2] + freqData [4] * 10000;
+		float mid = freqData [11] + freqData [12] + freqData [13] * 10000;
+		float midTwo = freqData [22] + freqData [23] + freqData [24] * 10000;
+		float high = freqData [44] + freqData [45] + freqData [46] + freqData [47] + freqData [48] + freqData [49] * 10000;
 		//print (low);
 		//print ("high: " + high);
 		spawner = false;
 		spawner2 = false;
 		spawner3 = false;
 		spawner4 = false;
-		freqLow = 0.004f;
-		freqMid = 0.009f;
-		freqMidTwo = 0.01f;
-		freqHigh = 0.01f;
 
 		counter += Time.deltaTime;
-		print (counter + ". mid: " + mid);
-
+		//print (audio.time);
+		print ("low: " + low + "- mid: " + mid + "- midTwo: " + midTwo + "- high: " + high);
+		if (counter >= 0) {
+			freqLow = 0.004f;
+			freqMid = 0.009f;
+			freqMidTwo = 0.01f;
+			freqHigh = 0.09f;	
+		}
 		if (counter >= 12.6) {
-			freqMid = 0.007f;		
+			freqMid = 0.0024f;		
 		}
 		if (counter >= 14.6) {
 			freqMid = 0.005f;
-			freqLow = 0.1f;
+			freqLow = 0.09f;
 		}
 
 		if (low >= freqLow) {
-			spawner = true;
+			//spawner = true;
 		}
-		if (mid >= freqMid) {
+		if (mid == 13.28075) {
 			spawner2 = true;
 		}
 		if (midTwo >= freqMidTwo) {
-			spawner3 = true;
+			//spawner3 = true;
 		}
 		if (high >= freqHigh) {
-			spawner4 = true;
-		}
+			//spawner4 = true;
+		}*/
 		/*freqData = audio.GetSpectrumData(nSamples, 0, FFTWindow.Hamming);
 
 		int i = 1;
@@ -74,4 +218,5 @@ public class soundCheck : MonoBehaviour {
 
 		}*/
 	}
+
 }
