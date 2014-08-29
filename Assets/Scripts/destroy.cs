8125fdf7f4ce6b8bf2a87b61ad861af185bbb7cb
Nolimet <nolimet@stamdata.nl>
2014-08-29 10:10:23 +0200

    Worked OnDestoryer for Objects

--- Assets/Scripts/destroy.cs
+++ Assets/Scripts/destroy.cs
@@ -2,34 +2,56 @@
 using System.Collections;
 
 public class destroy : MonoBehaviour {
-	void OnTriggerEnter2D(Collider2D enemy){
-		if (enemy.gameObject.tag == "Low")
-		{
-			if(Input.GetKey(KeyCode.Z)){
-				print("haha");
-				Destroy(this.gameObject);	
-			}
-		}
-		if (enemy.gameObject.tag == "Mid")
-		{
-			if(Input.GetKey(KeyCode.X)){
-				print("haha");
-				Destroy(this.gameObject);	
-			}
-		}
-		if (enemy.gameObject.tag == "MidTwo")
-		{
-			if(Input.GetKey(KeyCode.C)){
-				print("haha");
-				Destroy(this.gameObject);	
-			}
-		}
-		if (enemy.gameObject.tag == "High")
-		{
-			if(Input.GetKey(KeyCode.V)){
-				print("haha");
-				Destroy(this.gameObject);	
-			}	
-		}
-	}
+  /*  [SerializeField]
+    float punishTime;
+
+    void Update()
+    {
+        
+        if (punishTime > 0)
+            punishTime -= Time.deltaTime;
+    }
+    void OnTriggerEnter2D(Collider2D enemy)
+    {
+        if (punishTime > 0f)
+        {
+            if (enemy.gameObject.tag == "Low")
+            {
+                if (Input.GetKey(KeyCode.Z))
+                {
+                    Debug.Log("test");
+                    Score.addScore();
+                    Destroy(this.gameObject);
+                }
+            }
+            else if (enemy.gameObject.tag == "Mid")
+            {
+                if (Input.GetKey(KeyCode.X))
+                {
+                    Score.addScore();
+                    Destroy(this.gameObject);
+                }
+            }
+            else if (enemy.gameObject.tag == "MidTwo")
+            {
+                if (Input.GetKey(KeyCode.C))
+                {
+                    Score.addScore();
+                    Destroy(this.gameObject);
+                }
+            }
+            else if (enemy.gameObject.tag == "High")
+            {
+                if (Input.GetKey(KeyCode.V))
+                {
+                    Score.addScore();
+                    Destroy(this.gameObject);
+                }
+            }
+            else
+            {
+                punishTime = 0.2f;
+            }
+        }
+    }*/
 }
